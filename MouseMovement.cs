7b88481a94d1252cde78cd7c7495999ab06bce4d
yoddaa <49987506+yoddaa@users.noreply.github.com>
2021-10-05 00:21:49 +0900

    removing junk

--- MouseMovement.cs
+++ MouseMovement.cs
@@ -30,25 +30,4 @@ public class MouseMovement : MonoBehaviour
         }
     }
 
-
-    //void OnMouseDown()
-    //{
-    //    screenPoint = Camera.main.WorldToScreenPoint(scanPos);
-
-
-    //    offset = scanPos - Camera.main.ScreenToWorldPoint(
-    //        new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
-    //}
-
-
-    //void OnMouseDrag()
-    //{
-    //    Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-
-    //    Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-    //    transform.position = curPosition;
-
-    //}
 }
