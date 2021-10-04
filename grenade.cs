96fb333c9a5115c60746873ac475e87300a658ec
yoddaa <49987506+yoddaa@users.noreply.github.com>
2021-10-05 00:08:55 +0900

    removed un used code

--- grenade.cs
+++ grenade.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using TMPro;
-//using UnityEngine.UI;
+
 // A grenade
 // - instantiates an explosion Prefab when hitting a surface
 // - then destroys itself
@@ -12,7 +11,7 @@ public class grenade : MonoBehaviour
     public int score;
     public Transform explosionPrefab;
     public Transform pointsPrefab;
-    //public TMP_Text textMP;
+    
 
     void OnCollisionEnter(Collision collision)
     {
@@ -31,7 +30,7 @@ public class grenade : MonoBehaviour
             Vector3 position = contact.point;
             Instantiate(pointsPrefab, position, rotation);
             score++;
-            //textMP.text = score.ToString();
+           
             Destroy(gameObject);
         }
     }
